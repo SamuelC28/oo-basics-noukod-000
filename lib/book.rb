7c45a2168266df5f305eca350ce0347a2ce26349
Samuel Cornet <samuel@noukod.com>
2018-09-25 18:34:01 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+   def initialize(title)
+     @title = title
+   end
 
+   def title
+     @title
+   end
 
+  #  def authors_name
+  #  end
+   #
+  #  def page_count
+  #  end
+   #
+  #  def genre
+  #  end
+end
