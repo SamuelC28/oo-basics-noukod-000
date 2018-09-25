ef4d4ddaeab35cd77f915c4733a75341087836b4
Samuel Cornet <samuel@noukod.com>
2018-09-25 18:57:20 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,17 +17,17 @@ class Book
    def page_count=(page_count)
       @page_count = page_count
    end
-   
+
    def page_count
-      @page_count 
+      @page_count
    end
 
    def genre=(genre)
       @genre = genre
    end
-   
+
    def genre
-      @genre 
+      @genre
    end
   #  def genre
   #  end
